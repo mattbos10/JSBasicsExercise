653da7e494a10fd4be87d3f6813c009eaba950a6
Matt Boston <mattbos10@icloud.com>
2021-09-21 09:35:03 -0500

    all

---
+++ script.js
@@ -0,0 +1,41 @@
+// JS Basics Exercise
+// 1
+console.log(8);
+// 2
+// Numbers, strings, booleans, null & undefined (symbols also, but very uncommon)
+// 3
+// addition + subtraction - multiplication * division / exponential ** modulus %
+// 4
+console.log(5%4);
+console.log(5%2);
+// 5
+console.log('I\'M MARY POPPINS Y\'ALL');
+// 6
+console.log('Hawk' + 'eye');
+// 7
+console.log('Matt'.length + 'Boston'.length);
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log('\\');
+// 2
+console.log(5 ** 4 - 9);
+// 3
+console.log(('I'.length + 'Am'.length) * 'Iron'.length / 'Man'.length % 4);
+    // Steps explained below
+        // (1+2) * 4 / 3 % 4
+        // (3) * 4 / 3 % 4
+        // 12 / 3 % 4
+        // 4 % 4 = 0
+// 4
+console.log('CAP'[1]);
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('NICK FURY'[0]);
+console.log('GROOT'[0]);
+console.log('DOCTOR STRANGE'[13]);
+console.log('THOR'[3]);
+console.log('STAR-LORD'[0]);
+// 5
